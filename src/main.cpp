0bfac722c362622c8249d88e0ddf6a14eeb73c89
suurkivi <136639517+suurkivi@users.noreply.github.com>
2023-12-02 07:34:14 -0800

    send power message

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@
 
 #define NEO 11
 #define NEO_POWER 12
+#define INA219_ADDR 0x41
+
+#define RADIO_SRC_ADDR 0xC1
+#define RADIO_DST_ADDR 0x02
 
 
 Adafruit_AHTX0 *aht;
@@ -42,7 +46,7 @@ void setup() {
     blink(25, 25, 175);
 
     aht = new Adafruit_AHTX0();
-    ina219 = new Adafruit_INA219(0x41);
+    ina219 = new Adafruit_INA219(INA219_ADDR);
 
     if (!aht->begin()) {
         aht = null;
@@ -58,7 +62,7 @@ void setup() {
 
     radio = new RFM69(0, 1, true, &SPI);
 
-    if (!radio->initialize(RF69_433MHZ, 0xC0, 100)) {
+    if (!radio->initialize(RF69_433MHZ, RADIO_SRC_ADDR, 100)) {
         blinkForever(500, 100);
     }
 
@@ -66,8 +70,6 @@ void setup() {
 }
 
 int blinkCount = 10;
-
-void readCurrent();
 
 void readTemp() {
     sensors_event_t humidity, temp;
@@ -84,19 +86,36 @@ void readTemp() {
     packFloat(temp.temperature, buf + 1);
     packFloat(humidity.relative_humidity, buf + 5);
 
-    radio->send(
-            0x02,
-            buf,
-            sizeof(buf));
+    radio->send(RADIO_DST_ADDR, buf, sizeof(buf));
 }
 
-void readCurrent() {
+void readPower() {
     float v = ina219->getBusVoltage_V();
     float i = ina219->getCurrent_mA();
     float p = ina219->getCurrent_mA();
 
-    int vInt = int(v);
-    blink(vInt, 25, 475);
+    uint8_t buf[1 + 1 + 4 + 4 + 16];
+    memset(buf, 0, sizeof(buf));
+
+    uint8_t *pos = buf;
+    pos[0] = 0x03;
+    pos += 1;
+    pos[0] = INA219_ADDR;
+    pos += 1;
+
+    packFloat(v, pos);
+    pos += 4;
+
+    packFloat(i, pos);
+    pos += 4;
+
+    packFloat(p, pos);
+    pos += 4;
+
+    const char *description = "test-11";
+    memcpy(pos, description, strlen(description));
+
+    radio->send(RADIO_DST_ADDR, buf, sizeof(buf));
 }
 
 
@@ -106,7 +125,7 @@ void loop() {
     }
 
     if (ina219 != null) {
-        readCurrent();
+        readPower();
     }
 
     radio->setMode(RF69_MODE_SLEEP);
