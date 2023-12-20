d1cf82e7ce4f930ec68901dd5355ad4026df7665
suurkivi <136639517+suurkivi@users.noreply.github.com>
2023-12-20 09:37:44 -0800

    probe/no probe detection

--- src/main.cpp
+++ src/main.cpp
@@ -227,7 +227,6 @@ void readADC() {
         D.channels[i].V = ads1115->computeVolts(D.channels[i].counts);
     }
 
-
     for (int i = 0; i < 4; ++i) {
         Channel *ch = &D.channels[i];
         String info;
@@ -246,15 +245,27 @@ void readADC() {
 
     // actual temperature channels
     for (int i = 1; i < 3; ++i) {
+        String summary;
+        summary += "channel " + String(i);
+        summary += " ";
+
         Channel *ch0 = &D.channels[0];
         Channel *ch = &D.channels[i];
         const float r_ref = 20000;
 
         float r = float(ch0->counts) / float(ch->counts) * r_ref - r_ref;
+
+        if (r < 1e3 || r > 1e6) {
+            ch->present = false;
+            summary += "no probe";
+            Serial.println(summary);
+            continue;
+        }
+
+        ch->present = true;
         ch->T = shModel(r);
         float tF = ch->T * 9.0f / 5.0f + 32.0f;
 
-        String summary;
         summary += "r = " + String(r / 1000.0) + "k";
         summary += " ";
 
